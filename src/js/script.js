aebc6d689e857582f4ff90cfd13e87c26da46d02
divineamunega <114052520+divineamunega@users.noreply.github.com>
2023-08-03 11:02:09 +0100

    Doing Some javaScript

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-import * as bootstrap from "bootstrap";
+// import * as bootstrap from "bootstrap";
+
+const nameSubmitButton = document.querySelector(`.btn--submit-modal`);
+const firstNameInput = document.querySelector(".first-name-input");
+const lastNameInput = document.querySelector(`.lastNameInput`);
+
+class Account {
+	constructor(name) {
+		name = this.name;
+	}
+}
+class App {
+	#account;
+	constructor() {
+		nameSubmitButton.addEventListener(`click`, this.#submitName);
+	}
+
+	#submitName(e) {
+		this.#account = new Account();
+	}
+}
